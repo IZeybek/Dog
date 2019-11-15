cf746a6dde1b540ffe82047f3e2be6461208ef23
jpkmiller <j.mueller5021@gmail.com>
2019-11-15 09:25:19 +0100

    added some playerspec
    Signed-off-by: jpkmiller <j.mueller5021@gmail.com>

--- src/test/scala/model/playerComponent/PlayerSpec.scala
+++ src/test/scala/model/playerComponent/PlayerSpec.scala
@@ -1,22 +1,24 @@
 package model.playerComponent
 
-import model.playerComponent.card.{CardTrait, ChangeCard, JokerCard, SevenCard}
 import model.{Piece, Player}
 import org.scalatest.{Matchers, WordSpec}
 
 class PlayerSpec extends WordSpec with Matchers {
   "A player" when {
     "created" should {
-//      val cards: Array[CardTrait] = Array(SevenCard(), JokerCard(), ChangeCard())
-      val player = Player("PlayerSpec", "gelb",Map(0 -> Piece(0), 1 -> Piece(0), 2 -> Piece(0), 3 -> Piece(0)))
+      //      val cards: Array[CardTrait] = Array(SevenCard(), JokerCard(), ChangeCard())
+      val player = Player("PlayerSpec", "gelb", Map(0 -> Piece(0), 1 -> Piece(0), 2 -> Piece(0), 3 -> Piece(0)))
       "have a name" in {
         player.name should not be empty
       }
       "be printed" in {
         player.toString should be(player.name)
       }
-      "its color" in {
+      "have a color" in {
         player.getColor should be(player.color)
+      }
+      "have a map of pieces on the field" in {
+        player.getPiece(2).getPosition should be(0)
       }
     }
   }
