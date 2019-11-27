29b7c6180e6dcf7e55d1b3b4b86a13c51939b0ae
jpkmiller <j.mueller5021@gmail.com>
2019-11-27 16:57:04 +0100

    added possibility to draw and remove a Card
    Signed-off-by: jpkmiller <j.mueller5021@gmail.com>

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -9,21 +9,23 @@ case class Player(name: String, color: String, piece: Map[Int, Piece], inHouse: 
 
   def getPiece: Map[Int, Piece] = piece //indexing & mapping pieces
 
-  def getPosition(pieceNum: Integer): Integer = {
-    piece(pieceNum).getPosition
-  }
+  def getPosition(pieceNum: Integer): Integer = piece(pieceNum).getPosition
 
   def movePlayer(pieceNum: Integer, moveBy: Integer): Player = {
     val oldPos = piece(pieceNum).getPosition
     copy(piece = piece.updated(pieceNum, piece(pieceNum).movePiece(moveBy)), inHouse = if (oldPos == 0) inHouse - 1 else inHouse)
   }
 
-  def overridePlayer(pieceNum: Integer): Player = {
-    copy(piece = piece.updated(pieceNum, piece(pieceNum).setPosition(0)), inHouse = inHouse + 1)
-  }
+  def overridePlayer(pieceNum: Integer): Player = copy(piece = piece.updated(pieceNum, piece(pieceNum).setPosition(0)), inHouse = inHouse + 1)
+
+
+  def removeCard(card: Card, list: List[Card]): List[Card] = list diff List(card)
+
+  def drawCard(cardNum: Integer): Card = card(cardNum)
 
   override def toString: String = name
 }
+
 
 case class Piece(var position: Int) {
   def getPosition: Int = position
