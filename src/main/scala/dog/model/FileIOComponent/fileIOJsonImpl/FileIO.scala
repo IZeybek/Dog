e0508da33684029c35bff94ba0760ab2bbfaf1aa
jpkmiller <j.mueller5021@gmail.com>
2020-01-10 10:42:27 +0100

    started JSON
    Signed-off-by: jpkmiller <j.mueller5021@gmail.com>

--- src/main/scala/dog/model/FileIOComponent/fileIOJsonImpl/FileIO.scala
+++ src/main/scala/dog/model/FileIOComponent/fileIOJsonImpl/FileIO.scala
@@ -1,28 +1,30 @@
 package dog.model.FileIOComponent.fileIOJsonImpl
 
-import dog.controller.GameState
-import dog.model.FileIOComponent.FileIOTrait
-import dog.model.Player
-import play.api.libs.json.{JsValue, Json}
+//class FileIO extends FileIOTrait {
+//  override def load: GameState = ???
+//
+//
+//  override def save(gameState: GameState): Unit = {
+//    import java.io._
+//    val pw = new PrintWriter(new File("grid.json"))
+//    pw.write(Json.prettyPrint(gameStateToJson(gameState)))
+//    pw.close
+//  }
+//
 
-class FileIO extends FileIOTrait {
-  override def load: GameState = ???
-
-
-  override def save(gameState: GameState): Unit = {
-    import java.io._
-    val pw = new PrintWriter(new File("grid.json"))
-    pw.write(Json.prettyPrint(gameStateToJson(gameState)))
-    pw.close
-  }
-
-  def gameStateToJson(gameState: GameState): JsValue = {
-    Json.obj(
-      "player" -> Json.toJson(gameState.players._1.foreach(x => Json.toJson(playerToJson(x)))
-      )
-  }
-
-  def playerToJson(player: Player): JsValue = {
-
-  }
-}
+//  def gameStateToJson(gameState: GameState): JsValue = {
+//    Json.obj(fields =
+//      "players" -> Json.toJson(gameState.players._1.foreach(x => Json.toJson(x))
+//      )
+//  }
+//
+//  implicit val playerWrites: Writes[Player] = (player: Player) => Json.obj(fields =
+//    "nameAndIdx" -> player.nameAndIdx,
+//    "color" -> player.color,
+//    "piece" -> Json.toJson(
+//      player.piece.foreach(x => Json.obj(fields = "pos" -> x._2.pos))
+//    )
+//
+//  )
+//
+//}
